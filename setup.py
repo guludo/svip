a6698c47927d44068e438381f0034d971d300292
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2021-07-12 10:52:01 -0300

    setup: require packages for tests and coverage

--- setup.py
+++ setup.py
@@ -7,4 +7,10 @@ setuptools.setup(
     install_requires=[
         'semantic_version~=2.8',
     ],
+    extras_require={
+        'tests': [
+            'pytest~=6.2',
+            'coverage~=5.5',
+        ],
+    },
 )
