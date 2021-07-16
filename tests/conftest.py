646be699512d3fb9b32cf4276c84c6aaee33a2cd
Gustavo Sousa <gustavo.jo.sousa@gmail.com>
2021-07-15 21:14:42 -0300

    tests: add fixture merge_steps_dirs()

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
+import itertools
 import pathlib
+import shutil
 
 import pytest
 
@@ -11,3 +13,28 @@ def datadir():
 @pytest.fixture
 def valid_step_filenames_dir(datadir):
     return datadir / 'valid-step-filenames'
+
+
+@pytest.fixture
+def merge_steps_dirs(tmp_path):
+    """
+    Fixture that returns a factory to merge steps from one or more directories
+    into a new directory.
+
+    This provides the convenience of not needing to create a lot of files for a
+    test case: just create the step for the case you want to test and inherit
+    step script from another directory.
+    """
+    counter = itertools.count()
+    def factory(*paths):
+        dstdir = tmp_path / f'steps-{next(counter)}'
+        for p in paths:
+            if p is None:
+                continue
+            shutil.copytree(
+                p,
+                dstdir,
+                dirs_exist_ok=True,
+            )
+        return dstdir
+    return factory
